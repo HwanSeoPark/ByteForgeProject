f847b182fe7c05659e3b6a85b9f5ca6093a5a751
hye <hyekyung0610@naver.com>
2025-07-24 10:49:00 +0900

    Update /lecture

--- src/main/java/com/byteforge/lecture/controller/YoutubeController.java
+++ src/main/java/com/byteforge/lecture/controller/YoutubeController.java
@@ -1,7 +1,6 @@
 package com.byteforge.lecture.controller;
 
-import com.byteforge.lecture.dto.api.SearchVideosResponse;
-import com.byteforge.lecture.dto.view.CustomLectureVideoDto;
+import com.byteforge.lecture.dto.LectureDto;
 import com.byteforge.lecture.service.YoutubeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +11,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/search")
-public class YoutubeSearchController {
+public class YoutubeController {
+
     private final YoutubeService youtubeService;
 
-    // Youtube API 원본
-    @GetMapping
-    public SearchVideosResponse searchVideos(@RequestParam("query") String query) {
-        return youtubeService.callSearchVideos(query);
-    }
-
-    // Custom DTO
-    @GetMapping("/custom")
+    @GetMapping()
     public ResponseEntity<?> searchLecture(@RequestParam("query") String query) {
-        try{
-            List<CustomLectureVideoDto> result = youtubeService.getLectureDto(query);
+        try {
+            List<LectureDto> result = youtubeService.getLectureDto(query);
             return ResponseEntity.ok(result);
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
